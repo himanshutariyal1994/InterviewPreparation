aabcf07c1f3948b843b49a7f3cd11dfd54cc5e3b
Himanshu Tariyal <himanshu.tariyal@axtria.com>
2022-02-22 11:07:39 +0530

    Added Day 0 and 1 for 30 Days of Code Challenge

--- 136 - Single Number/Solution.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        result = 0
-
-        for i in range(0, len(nums)):
-            result ^= nums[i]
-        return result
